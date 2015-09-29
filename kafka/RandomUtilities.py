9543ec34b984cf9a093eb793c689a0f124df2213
Mae Qiu <mae.x.qiu@gmail.com>
2015-09-29 00:40:54 -0700

    First commit

---
+++ kafka/RandomUtilities.py
@@ -0,0 +1,97 @@
+'''
+Created on Sep 14, 2015
+
+@author: maeq
+'''
+import json
+import random
+import os
+import datetime
+import time
+import math
+from faker import Factory
+
+def strTimeProp(start, end, format, prop):
+    """Get a time at a proportion of a range of two formatted times.
+
+    start and end should be strings specifying times formated in the
+    given format (strftime-style), giving an interval [start, end].
+    prop specifies how a proportion of the interval to be taken after
+    start.  The returned time will be in the specified format.
+    """
+
+    stime = time.mktime(time.strptime(start, format))
+    etime = time.mktime(time.strptime(end, format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(format, time.localtime(ptime))
+
+def randomDate(start, end, prop):
+    return strTimeProp(start, end, '%Y,%m,%d,%H,%M,%S', prop)
+
+def randomLatLon(lat, lon, radius, num_rows):
+    """ generate random Lat Long positions within radium(meters) from (lat,lon)
+    number of Lat Long to be generated equals num_rows
+    """
+    radiusInDegrees=radius/111300.           
+    r = radiusInDegrees
+    x0 = lat
+    y0 = lon
+    latlist=[]
+    lonlist=[]
+    
+    for _ in range(num_rows):
+    
+        u = float(random.uniform(0.0,1.0))
+        v = float(random.uniform(0.0,1.0))
+        
+        w = r * math.sqrt(u)
+        t = 2 * math.pi * v
+        x = w * math.cos(t) 
+        y = w * math.sin(t)
+          
+        xLat  = x + x0
+        yLong = y + y0
+        latlist.append(xLat)
+        lonlist.append(yLong)
+        
+    return latlist, lonlist
+
+def create_json_message(name, latdep, longdep, latarr, longarr, timestamp, dr_flag, user_id, message_id):
+    """Create JSON messages with different message fields
+    
+    Args:
+        name: a person's name ("Jane Doe"), string
+        latdep: departing latitude, double
+        longdep: departing longitude, double
+        latarr: arriving latitude, double
+        longarr: arriving longitude, double
+        timestamp: the timestamp when the message was sent in the format of
+                  "year, month, day, hour, minute, second", string
+        dr_flag: flag indicating it's driver or rider (1 for driver, 0 for rider), integer
+        user_id: user ID, integer
+        
+    Returns:
+        A string in JSON format
+        example:
+        {'name': "Jane Doe",
+         'latdep': 37.426327,
+         'longdep': -122.141076,
+         "longarr": -121.650043, 
+         "latarr": 38.142658,
+         'timestamp': "2015, 2, 8, 12, 13, 49",
+         'dr_flag': 1,
+         'user_id': 00001}
+    """
+    message_info = {"name": name,
+                    "latdep": latdep,
+                    "longdep": longdep,
+                    "latarr": latarr,
+                    "longarr": longarr,
+                    "timestamp": timestamp,
+                    "drFlag": dr_flag,
+                    "userID": user_id,
+                    "messageID": message_id}
+
+    return json.dumps(message_info)
---
+++ kafka/kafka_producer.py
@@ -0,0 +1,63 @@
+'''
+Created on Sep 15, 2015
+
+@author: maeq
+'''
+import random
+import sys
+import six
+import time
+import logging
+import json
+from faker import Factory
+import RandomUtilities as rutil
+from datetime import datetime
+from kafka.client import KafkaClient
+from kafka.producer import SimpleProducer
+
+fake = Factory.create()
+NUM_USERS = 30
+RADIUS = 50000
+LATDEP = 37.061283
+LONGDEP = -120.847512
+LATARR = 37.957006
+LONGARR = -121.291664
+
+#logging.basicConfig(
+#    format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
+#    level=logging.DEBUG
+#)
+
+class Producer(object):
+
+    def __init__(self, addr):
+        self.client = KafkaClient(addr)
+        self.producer = SimpleProducer(self.client)
+        self.latlist1, self.lonlist1 = rutil.randomLatLon(LATDEP, LONGDEP, RADIUS, NUM_USERS)
+        self.latlist2, self.lonlist2 = rutil.randomLatLon(LATARR, LONGARR, RADIUS, NUM_USERS)
+
+    def produce_msgs(self):
+        
+        msg_cnt = 0
+        while msg_cnt<NUM_USERS:
+            timestamp=rutil.randomDate("2015,09,01,00,00,00", "2015,09,08,23,59,00", random.random())
+            name = fake.name()
+
+            message_info = rutil.create_json_message(name=name,
+                                           latdep=self.latlist1[msg_cnt],
+                                           longdep=self.lonlist1[msg_cnt],
+                                           latarr=self.latlist2[msg_cnt],
+                                           longarr=self.lonlist2[msg_cnt],
+                                           timestamp=timestamp,
+                                           dr_flag=random.choice([0, 1]),
+                                           user_id=random.choice(range(NUM_USERS)),
+                                           message_id=msg_cnt)
+            print message_info
+            self.producer.send_messages('test',message_info)
+            msg_cnt += 1
+
+if __name__ == "__main__":
+    args = sys.argv
+    ip_addr = str(args[1])
+    prod = Producer(ip_addr)
+    prod.produce_msgs() 
